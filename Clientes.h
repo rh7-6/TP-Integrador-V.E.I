10dcf5a25b5da8cd59feef3a3ee7f2f60e41c998
rh7 <rodolfohess766@gmail.com>
2025-05-31 19:30:28 -0300

    .

--- Clientes.h
+++ Clientes.h
@@ -1,6 +1,6 @@
 #ifndef CLIENTES_H_INCLUDED
 #define CLIENTES_H_INCLUDED
-
+#include "cadena.h"
 
 class Cliente {
 public:
@@ -8,6 +8,11 @@ public:
 
 
 private:
+
+    Cadena _Cuit;
+    Cadena _Nombre;
+    Cadena _Apellido;
+
 
 };
 #endif // CLIENTES_H_INCLUDED
