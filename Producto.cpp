bfab53aa83b806f53be5248cb443de3c8eb818df
tony <tomas.prenezio@alumnos.frgp.utn.edu.ar>
2025-05-31 20:28:35 -0300

    .
    revisar

--- Producto.cpp
+++ Producto.cpp
@@ -1,3 +1,85 @@
 #include <iostream>
 using namespace std;
+#include "Producto.h"
 
+void Producto::set_numero_de_producto(int num)
+{
+    bool a=true;
+    while(a==true)
+    {
+        if(num<=0)
+        {
+            cout<<"el numero tiene que ser mayor o igual a 0 por favor vuelva a cargar el numero"<<endl;
+            a=true;
+        }
+        else
+        {
+            a=false;
+            numero_de_producto=a;
+        }
+    }
+}
+
+void Producto::set_precio(float num)
+{
+    bool a=true;
+    while(a==true)
+    {
+        if(num<=100)
+        {
+            cout<<"el precio tiene que ser minimo 100 por favor vuelva a cargar el precio"<<endl;
+            a=true;
+        }
+        else
+        {
+            a=false;
+            precio=a;
+        }
+    }
+}
+
+void Producto::set_descripcion(const char palabra)
+{
+    ///descripcion=cargar cadena///
+}
+
+void Producto::set_marca(const char palabra)
+{
+    ///marca=cargar cadena///
+}
+
+void Producto::set_tipo_de_equipo(int num)
+{
+  bool a=true;
+    while(a==true)
+    {
+        if(num==1||num==2)
+        {
+            cout<<"el tipo de equipo tiene que ser de 1-productos o 2-equipos"<<endl;
+            a=true;
+        }
+        else
+        {
+            a=false;
+            precio=a;
+        }
+    }
+}
+
+void Producto::set_stock(int num)
+{
+    bool a=true;
+    while(a==true)
+    {
+        if(num<=0)
+        {
+            cout<<"el stock no puede ser menor a 0"<<endl;
+            a=true;
+        }
+        else
+        {
+            a=false;
+            precio=a;
+        }
+    }
+}
--- Producto.h
+++ Producto.h
@@ -1,12 +1,23 @@
 #ifndef PRODUCTO_H_INCLUDED
 #define PRODUCTO_H_INCLUDED
 
-
 class Producto {
 public:
-
+void set_numero_de_producto(int num);
+void set_precio(float num);
+void set_descripcion(const char palabra);
+void set_marca(const char palabra);
+void set_tipo_de_equipo(int num);
+void set_stock(int num);
 
 private:
+
+    int numero_de_producto;
+    float precio;
+    const char descripcion[50];
+    const char marca[20];
+    int tipo_de_equipo;
+    int stock;
 
 };
 #endif // PRODUCTO_H_INCLUDED
