69d9bbad6f279c7f04c40a4175f9fb6fb84d5dc6
rh7 <rodolfohess766@gmail.com>
2025-06-01 18:34:06 -0300

    declaración Venta

--- Venta.cpp
+++ Venta.cpp
@@ -1,4 +1,40 @@
 #include <iostream>
+#include "Venta.h"
+#include "cadena.h"
+#include "Fecha.h"
+
 using namespace std;
 
 
+    void SetIdVenta(int id){
+
+    }
+
+    void SetCuit(const char *t){
+
+    }
+
+    void SetFechaVenta(Fecha f){
+
+    }
+
+    void SetImporteVenta(float v){
+
+    }
+
+
+    int GetIdVenta(){
+
+    }
+
+    const char *GetCuit(){
+
+    }
+
+    Fecha GetFecha(){
+
+    }
+
+    float GetImporteVenta(){
+
+    }
--- Venta.h
+++ Venta.h
@@ -1,15 +1,30 @@
 #ifndef VENTA_H_INCLUDED
 #define VENTA_H_INCLUDED
 #include "cadena.h"
+#include "Fecha.h"
 
 
 class Venta {
 public:
 
+    void SetIdVenta(int id);
+    void SetCuit(const char *t);
+    void SetFechaVenta(Fecha f);
+    void SetImporteVenta(float v);
+
+    int GetIdVenta();
+    const char *GetCuit();
+    Fecha GetFecha();
+    float GetImporteVenta();
+
 
 private:
 
-    int _IdVenta
+    int _IdVenta;
+    Cadena _Cuit;
+    Fecha _Fecha;
+    float _ImporteVenta;
+
 
 };
 #endif // VENTA_H_INCLUDED
