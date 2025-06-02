226646111153eba06239a2443e4ad8ed018c45e6
rh7 <rodolfohess766@gmail.com>
2025-06-01 22:27:42 -0300

    .

--- Archivo_Clientes.h
+++ Archivo_Clientes.h
@@ -1,12 +1,15 @@
 #ifndef ARCHIVO_CLIENTES_H_INCLUDED
 #define ARCHIVO_CLIENTES_H_INCLUDED
-
+#include "cadena.h"
 
 class ArchivoClientes {
 public:
 
 
+
 private:
+
+    Cadena _NombreArchivo;
 
 };
 #endif // ARCHIVO_CLIENTES_H_INCLUDED
