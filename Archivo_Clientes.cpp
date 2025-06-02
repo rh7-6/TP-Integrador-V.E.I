87c1371a153b614939db44558098ab9a8cd82b8d
rh7 <rodolfohess766@gmail.com>
2025-06-01 22:56:38 -0300

    declaración archivo clientes

--- Archivo_Clientes.cpp
+++ Archivo_Clientes.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
+#include "Archivo_Clientes.h"
 using namespace std;
 
+
+    void GuardarCliente(){
+
+    }
+
+    int BuscarCliente(){
+
+    }
+
+    void MostrarCliente(int pos){
+
+    }
+
+    Cliente LeerCliente(int pos){
+
+    }
+
+    /// pendiente desarrollar el constructor para crear el archivo .dat
--- Archivo_Clientes.h
+++ Archivo_Clientes.h
@@ -1,12 +1,17 @@
 #ifndef ARCHIVO_CLIENTES_H_INCLUDED
 #define ARCHIVO_CLIENTES_H_INCLUDED
 #include "cadena.h"
+#include "Clientes.h"
 
 class ArchivoClientes {
 public:
 
+    void GuardarCliente();
+    int BuscarCliente();
+    void MostrarCliente(int pos);
+    Cliente LeerCliente(int pos);
 
-
+    /// pendiente declarar constructor para crear el archivo .dat
 private:
 
     Cadena _NombreArchivo;
