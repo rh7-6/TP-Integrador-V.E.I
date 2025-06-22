362d31ad4125daebaa9093d75df8cfcba6c0d294
tony <tomas.prenezio@alumnos.frgp.utn.edu.ar>
2025-06-22 15:43:05 -0300

    Banana
    menu finalizado

--- main.cpp
+++ main.cpp
@@ -21,21 +21,21 @@ bool bandera=true;
 do{
 menu();
 rlutil::hidecursor();
-rlutil::locate(57,10+y);
+rlutil::locate(54,10+y);
 cout<<(char)175<<endl;
 icono = rlutil::getkey();
 switch (icono){
     case(14):///arriba///
-    rlutil::locate(57,10+y);
+    rlutil::locate(54,10+y);
     cout<<" "<<endl;
     y--;
-    if(y<0){y=3;}
+    if(y<0){y=5;}
     break;
     case(15):///abajo///
-    rlutil::locate(57,10+y);
+    rlutil::locate(54,10+y);
     cout<<" "<<endl;
     y++;
-    if(y>3){y=0;}
+    if(y>5){y=0;}
     break;
     ///rlutil::showcursor(); hace aparecer el cursor///
     case(1):///enter///
@@ -123,9 +123,9 @@ switch (icono){
             system("cls");
             break;
 
-            case(2):
+            case(2)://////////////////////INFORMES////////////////////////////
 
-        system("cls");
+            system("cls");
             do{
             MenuInformes();
             rlutil::locate(43,10+a);
@@ -166,9 +166,121 @@ switch (icono){
             system("cls");
 
         break;
-        case(3):
+        case(3)://////////////////////////CARGA UN REGISTO////////////////////
+
+            system("cls");
+            do{
+            MenuRegistro();
+            rlutil::locate(43,10+a);
+            cout<<(char)175<<endl;
+            icono = rlutil::getkey();
+            switch (icono){
+                case(14):///arriba///
+                rlutil::locate(43,10+a);
+                cout<<" "<<endl;
+                a--;
+                if(a<0){a=4;}
+                break;
+                case(15):///abajo///
+                rlutil::locate(43,10+a);
+                cout<<" "<<endl;
+                a++;
+                if(a>4){a=0;}
+                break;
+                case(1):///enter///
+                    Cliente cl;
+                    Venta ven;
+                    Producto prod;
+                    DetalleVenta vendet;
+                    switch(a)
+                    {
+                        case(0):
+                            system("cls");
+                            rlutil::showcursor();
+                            cl.CargarCliente();
+                            system("cls");
+////////////////////////////////////////////guarda registro de cliente/////////////////////////////////////////////////////////
+                        break;
+                        case(1):
+                            system("cls");
+                            rlutil::showcursor();
+                            ven.CargarVenta();
+                            system("cls");
+////////////////////////////////////////////guarda registro de venta/////////////////////////////////////////////////////////
+                        break;
+                        case(2):
+                            system("cls");
+                            rlutil::showcursor();
+                            prod.CargarProducto();
+                            system("cls");
+////////////////////////////////////////////guarda registro de producto/////////////////////////////////////////////////////////
+                        break;
+                        case(3):
+
+                            system("cls");
+                            rlutil::showcursor();
+                            vendet.CargarDetalleVenta();
+                            system("cls");
+////////////////////////////////////////////guarda registro detalle de venta/////////////////////////////////////////////////////////
+                        break;
+                        case(4):
+                        bandera1=true;
+                        break;
+        break;
+        }
+        }
+        }while(bandera1==false);
+            system("cls");
+
+        break;
+
+        case(4)://////////////////////////MOSTRAR REGISTROS///////////////////
+
+            system("cls");
+            do{
+            MenuRegistroMostrar();
+            rlutil::locate(43,10+a);
+            cout<<(char)175<<endl;
+            icono = rlutil::getkey();
+            switch (icono){
+                case(14):///arriba///
+                rlutil::locate(43,10+a);
+                cout<<" "<<endl;
+                a--;
+                if(a<0){a=4;}
+                break;
+                case(15):///abajo///
+                rlutil::locate(43,10+a);
+                cout<<" "<<endl;
+                a++;
+                if(a>4){a=0;}
+                break;
+                case(1):///enter///
+                    switch(a)
+                    {
+                        case(0):
+                        break;
+                        case(1):
+                        break;
+                        case(2):
+                        break;
+                        case(3):
+                        break;
+                        case(4):
+                        bandera1=true;
+                        break;
+        break;
+        }
+        }
+        }while(bandera1==false);
+            system("cls");
+
+        break;
+
+        case(5)://////////////////////////SALIDA//////////////////////////////
         bandera=false;
         break;
+
         }
         break;
 
